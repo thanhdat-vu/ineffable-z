f4609679fb9d9eea5ec132897faaad3c994c73e6
Daniel <20521181@gm.uit.edu.vn>
2023-02-24 08:52:18 +0700

    feat: create Breadcrumb component

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+import { UrlObject } from "url";
+
+interface props {
+  items: { label: string; link: string | UrlObject }[];
+}
+
+const Breadcrumb = ({ items }: props) => {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol>
+        {items.map((item, index) => {
+          const isLast = index === items.length - 1;
+          return (
+            <li className="inline" key={index}>
+              {isLast ? (
+                item.label
+              ) : (
+                <Link
+                  href={item.link}
+                  className={`${!isLast && "text-white/50 hover:text-white"}`}
+                >
+                  {item.label}
+                </Link>
+              )}
+              {!isLast && " / "}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
--- src/pages/cocktails.tsx
+++ src/pages/cocktails.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, NextPage } from "next";
 import { Layout, RecipeCard } from "../components";
 import { getAllCocktails } from "lib/api";
 import metadata from "json/metadata.json";
+import Breadcrumb from "components/Breadcrumb";
 
 interface props {
   allCocktails?: any[];
@@ -11,14 +12,25 @@ const Cocktails: NextPage = ({ allCocktails }: props) => {
   return (
     <Layout metadata={metadata.cocktails}>
       <div className="my-24 lg:my-32 | w-max mx-auto">
-        {/* Title */}
+        <Breadcrumb
+          items={[
+            {
+              label: "Home",
+              link: "/",
+            },
+            {
+              label: "Cocktails",
+              link: "/cocktails",
+            },
+          ]}
+        />
+
         <div className="w-max mx-auto">
-          <h2 className="mb-16 | text-gold text-xl sm:text-2xl font-bold">
+          <h2 className="my-8 md:my-16 | text-gold text-xl sm:text-2xl font-bold">
             All Cocktails {allCocktails && `(${allCocktails.length})`}
           </h2>
         </div>
 
-        {/* Cocktail Cards */}
         <div className="gap-x-12 sm:gap-x-16 xl:gap-x-32 gap-y-6 sm:gap-y-12 xl:gap-y-16 | grid grid-cols-2 lg:grid-cols-4">
           {allCocktails
             ? allCocktails.map((cocktail) => (
